3d6c36605cbd8a0a08a3624e9968f97547eae91a
Shivani Vishwakarma <svshivanivishwakarma@gmail.com>
2024-06-12 12:08:43 +0200

    Added swagger

--- src/main/java/com/example/apidemo/entities/Product.java
+++ src/main/java/com/example/apidemo/entities/Product.java
@@ -1,15 +1,13 @@
 package com.example.apidemo.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -19,7 +17,9 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long Id;
+    @Column
     private String name;
+    @Column
     private Double price;
 
     public Long getId() {
